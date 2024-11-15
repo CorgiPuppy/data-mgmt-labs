ee7fc0fef7b4c32bf803e3a5f58537e68762433d
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2024-11-15 03:02:09 +0300

    feat: insert data to tables.

---
+++ lab5/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO groups (group_name) VALUES 
+	('CS-10'), 
+	('CS-14');
+
+INSERT INTO rooms (room_number) VALUES
+	(14),
+	(37),
+	(25);
+
+INSERT INTO students (student_name, scholarship, group_name) VALUES
+	('Vasilyev', 2000, 1),
+	('Petrova', 2570, 2),
+	('Sidorov', 2000, 2),
+	('Ivanov', 2240, 1),
+	('Sidorova', 4500, 1),
+	('Grishin', 4000, 2);
+
+INSERT INTO accommodations (accommodation_date, distance, room_number, student_name, neighbour_name) VALUES
+	('2005-08-03', 200, 1, 1, 6),
+	('2005-08-15', 435, 2, 2, 5),
+	('2005-08-02', 112, 3, 3, 4),
+	('2005-08-02', 240, 3, 4, 3),
+	('2005-08-14', 1200, 2, 5, 2),
+	('2005-08-04', 780, 1, 6, 1);
